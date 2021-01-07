d3ff7ab43d46eea46a3d74c8800f4d1a99b83ebd
Liam Nelski <ljnelski@gmail.com>
2021-01-06 20:28:02 -0500

    Created new gamobject parent for the Creature to have better origin

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // private vars
+    private Rigidbody2D rbody;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rbody.velocity.x));
+    }
+    /* Note for the walk animation to make sure the feet don't slide the formula is (0.3 * Speed) */
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // inspecter vars
+    [SerializeField] private float speed;
+    // private vars
+    private Rigidbody2D rbody;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(horiz) > 0.01f)
+        {
+            rbody.velocity = new Vector2(speed * horiz, rbody.velocity.y);
+        }
+        ChangeDirection(false);
+    }
+    private void ChangeDirection(bool flip)
+    {
+        if (flip)
+            transform.parent.localScale *= -1f;
+        else
+        {
+            if (rbody.velocity.x > 0.1f)
+                transform.parent.localScale = new Vector2(-1f, 1f);
+            else if (rbody.velocity.x < -0.1f)
+                transform.parent.localScale = new Vector2(1f, 1f);
+        }
+    }
+}
